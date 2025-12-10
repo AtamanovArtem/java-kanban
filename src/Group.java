a5c983575e9f674d0eab23b2005094e45e444713
AtamanovArtem <aassvpz@mail.ru>
2025-12-10 21:47:12 +0300

    Полностью добавлен код программы

---
+++ src/Group.java
@@ -0,0 +1,14 @@
+public class Group {
+    //название группы
+    private String title;
+    //тип (взрослая или детская)
+    private Age age;
+    //длительность (в минутах)
+    private int duration;
+
+    public Group(String title, Age age, int duration) {
+        this.title = title;
+        this.age = age;
+        this.duration = duration;
+    }
+}
---
+++ src/Timetable.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Timetable {
+
+    private Map<DayOfWeek, TreeMap<TimeOfDay, ArrayList<TrainingSession>>> timetable = new HashMap<>();
+
+    public void addNewTrainingSession(TrainingSession trainingSession) {
+        DayOfWeek dayOfWeek = trainingSession.getDayOfWeek();
+        TimeOfDay timeOfDay = trainingSession.getTimeOfDay();
+
+        if (!timetable.containsKey(dayOfWeek)) {
+            timetable.put(dayOfWeek, new TreeMap<>());
+        }
+
+        TreeMap<TimeOfDay, ArrayList<TrainingSession>> sessionsForDay = timetable.get(dayOfWeek);
+
+        if (sessionsForDay.containsKey(timeOfDay)) {
+            sessionsForDay.get(timeOfDay).add(trainingSession);
+        } else {
+            ArrayList<TrainingSession> sessions = new ArrayList<>();
+            sessions.add(trainingSession);
+            sessionsForDay.put(timeOfDay, sessions);
+        }
+    }
+
+
+    public TreeMap<TimeOfDay, ArrayList<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        if (timetable.containsKey(dayOfWeek)) {
+            return timetable.get(dayOfWeek);
+        } else {
+            return new TreeMap<>();
+        }
+    }
+
+    public ArrayList<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        if (timetable.containsKey(dayOfWeek)) {
+            TreeMap<TimeOfDay, ArrayList<TrainingSession>> trainingSessions = timetable.get(dayOfWeek);
+            ArrayList<TrainingSession> result = new ArrayList<>();
+            if (trainingSessions.containsKey(timeOfDay)) {
+                result.addAll(trainingSessions.get(timeOfDay));
+            }
+            return result;
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    public ArrayList<CounterOfTrainings> getCountByCoaches(ArrayList<TrainingSession> trainingSessions) {
+        Map<Coach, Integer> coachCount = new HashMap<>();
+        for (TrainingSession session : trainingSessions) {
+            Coach coach = session.getCoach();
+            coachCount.put(coach, coachCount.getOrDefault(coach, 0) + 1);
+        }
+        ArrayList<CounterOfTrainings> counterOfTrainingsList = new ArrayList<>();
+        for (Map.Entry<Coach, Integer> entry : coachCount.entrySet()) {
+            CounterOfTrainings counter = new CounterOfTrainings(entry.getKey(), entry.getValue());
+            counterOfTrainingsList.add(counter);
+        }
+        counterOfTrainingsList.sort(Comparator.comparingInt(CounterOfTrainings::getCount).reversed());
+        return counterOfTrainingsList;
+    }
+}
+
