1e3ee7c2bffe7ab17408b8b7948705bf29a4425f
AtamanovArtem <aassvpz@mail.ru>
2025-12-10 20:32:51 +0300

    001

---
+++ src/TrainingSession.java
@@ -0,0 +1,54 @@
+public class TrainingSession implements Comparable<TrainingSession> {
+
+    //группа
+    private Group group;
+    //тренер
+    private Coach coach;
+    //день недели
+    private DayOfWeek dayOfWeek;
+    //время начала занятия
+    private TimeOfDay timeOfDay;
+
+    public TrainingSession(Group group, Coach coach, DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        this.group = group;
+        this.coach = coach;
+        this.dayOfWeek = dayOfWeek;
+
+        if (timeOfDay.getHours() < 0 || timeOfDay.getHours() > 23) {
+            System.out.println("Часы должны быть в диапазоне от 0 до 23");
+        }
+        if (timeOfDay.getMinutes() < 0 || timeOfDay.getMinutes() > 59) {
+            System.out.println("Минуты должны быть в диапазоне от 0 до 59");
+        }
+
+        this.timeOfDay = timeOfDay;
+    }
+
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public Coach getCoach() {
+        return coach;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public TimeOfDay getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    @Override
+    public int compareTo(TrainingSession other) {
+
+        int dayComparison = this.dayOfWeek.compareTo(other.dayOfWeek);
+        if (dayComparison != 0) {
+            return dayComparison;
+        }
+
+        return this.timeOfDay.compareTo(other.timeOfDay);
+    }
+}
